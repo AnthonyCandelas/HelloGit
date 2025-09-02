9e8d03d2cea494b8f1e18683528045fe7974db9c
Anthony Candelas <tsallinger2@unl.edu>
2025-09-01 19:24:19 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,21 @@
+package Hello.java;
+
+/**
+ * Author: Anthony Candelas
+ * email: tsallinger2@unl.edu
+ * 
+ * 2025-09-01
+ * 
+ * This hack is for github connectivity with a lab partner. 
+ */
+
+
+public class hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Computer Science!");
+
+	}
+
+}
